ebe15c12447a469cd5de29fe68e5141f72e046e2
JonasHaas <haas.qt@gmail.com>
2024-06-11 11:22:01 +0200

    bugfix phase order

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,8 @@ import { defaultSettings } from '/src/defaults';
 export default function Home() {
 
   const [settings, setSettings] = useState(defaultSettings);
-  const [currentPhase, setCurrentPhase] = useState('standing');
-  const [timeRemaining, setTimeRemaining] = useState(settings.standingTime * 60); // in seconds
+  const [currentPhase, setCurrentPhase] = useState('sitting');
+  const [timeRemaining, setTimeRemaining] = useState(settings.sittingTime * 60); // in seconds
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
   const audioRef = useRef(null);
@@ -27,7 +27,7 @@ export default function Home() {
         breakTime: parseInt(savedBreakTime),
       };
       setSettings(newSettings);
-      setTimeRemaining(newSettings.standingTime * 60);
+      setTimeRemaining(newSettings.sittingTime * 60);
     }
   }, []);
 
@@ -51,15 +51,15 @@ export default function Home() {
   }, [isRunning]);
 
   const handleNextPhase = () => {
-    if (currentPhase === 'standing') {
-      setCurrentPhase('sitting');
-      setTimeRemaining(settings.sittingTime * 60);
-    } else if (currentPhase === 'sitting') {
+    if (currentPhase === 'sitting') {
+      setCurrentPhase('standing');
+      setTimeRemaining(settings.standingTime * 60);
+    } else if (currentPhase === 'standing') {
       setCurrentPhase('break');
       setTimeRemaining(settings.breakTime * 60);
     } else {
-      setCurrentPhase('standing');
-      setTimeRemaining(settings.standingTime * 60);
+      setCurrentPhase('sitting');
+      setTimeRemaining(settings.sittingTime * 60);
     }
     setIsRunning(false);
   };
@@ -74,10 +74,10 @@ export default function Home() {
 
   const handleSkip = () => handleNextPhase();
   const handleRedo = () => {
-    if (currentPhase === 'standing') {
-      setTimeRemaining(settings.standingTime * 60);
-    } else if (currentPhase === 'sitting') {
+    if (currentPhase === 'sitting') {
       setTimeRemaining(settings.sittingTime * 60);
+    } else if (currentPhase === 'standing') {
+      setTimeRemaining(settings.standingTime * 60);
     } else {
       setTimeRemaining(settings.breakTime * 60);
     }
@@ -92,10 +92,10 @@ export default function Home() {
 
   const getProgressValue = () => {
     const totalTime =
-      currentPhase === 'standing'
-        ? settings.standingTime * 60
-        : currentPhase === 'sitting'
-          ? settings.sittingTime * 60
+      currentPhase === 'sitting'
+        ? settings.sittingTime * 60
+        : currentPhase === 'standing'
+          ? settings.standingTime * 60
           : settings.breakTime * 60;
     return ((totalTime - timeRemaining) / totalTime) * 100;
   };
